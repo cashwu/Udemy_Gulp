97ed9f70d134249b89f373e7bfa9376cdd892d0e
cash <cash@cashwu.com>
2017-10-02 23:36:12 +0800

    10. 加入 Jade Template Language

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require("gulp");
+var jade = require('gulp-jade');
 
 gulp.task("copyHTML", function(){
     return gulp.src("./source/**/*.html")
         .pipe(gulp.dest("./public/"))
 })
+
+gulp.task('jade', function() {
+   
+    gulp.src("./source/*.jade")
+      .pipe(jade({
+        pretty:true
+      }))
+      .pipe(gulp.dest("./public/"))
+});
