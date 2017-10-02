a805265314cb17b333a7a1fce4cacd630b70aa5b
cash <cash@cashwu.com>
2017-10-02 23:54:26 +0800

    12 Node Sass 高速編譯你的 SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
 var gulp = require("gulp");
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task("copyHTML", function(){
     return gulp.src("./source/**/*.html")
+    .pipe(plumber())
         .pipe(gulp.dest("./public/"))
 })
 
 gulp.task('jade', function() {
-   
     gulp.src("./source/*.jade")
+        .pipe(plumber())
       .pipe(jade({
         pretty:true
       }))
       .pipe(gulp.dest("./public/"))
 });
+
+gulp.task('sass', function () {
+    gulp.src('./source/scss/**/*.scss')
+    .pipe(plumber())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./source/scss/**/*.scss', ['sass']);
+    gulp.watch('./source/*.jade', ['jade']);
+});
+
+gulp.task("default", ["jade", "sass", "watch"]);
