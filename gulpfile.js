52665c0a0913e3e2f8772a72de63d3218450df84
cash <cash@cashwu.com>
2017-10-04 23:43:29 +0800

    13 PostCSS 更高明的 CSS 優化流程

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,39 @@ var jade = require('gulp-jade');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 
-gulp.task("copyHTML", function(){
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task("copyHTML", function() {
     return gulp.src("./source/**/*.html")
-    .pipe(plumber())
+        .pipe(plumber())
         .pipe(gulp.dest("./public/"))
 })
 
 gulp.task('jade', function() {
     gulp.src("./source/*.jade")
         .pipe(plumber())
-      .pipe(jade({
-        pretty:true
-      }))
-      .pipe(gulp.dest("./public/"))
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest("./public/"))
 });
 
-gulp.task('sass', function () {
-    gulp.src('./source/scss/**/*.scss')
-    .pipe(plumber())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./public/css'));
+gulp.task('sass', function() {
+
+    var plugins = [
+        autoprefixer({browsers: ['last 2 version', '> 5%', 'ie 8']})
+    ];
+
+    return gulp.src('./source/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        // 編譯完成 css
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch('./source/scss/**/*.scss', ['sass']);
     gulp.watch('./source/*.jade', ['jade']);
 });
